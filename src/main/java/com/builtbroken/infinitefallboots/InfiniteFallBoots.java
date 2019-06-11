0a78a45b85eaa7ed34ceac86fdc3bcf0b0df596d
itsmeow <mods.itsmeow@gmail.com>
2019-06-11 18:41:14 -0400

    Update to 1.14

--- src/main/java/com/builtbroken/infinitefallboots/InfiniteFallBoots.java
+++ src/main/java/com/builtbroken/infinitefallboots/InfiniteFallBoots.java
@@ -1,87 +1,112 @@
 package com.builtbroken.infinitefallboots;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.lang3.tuple.Pair;
 
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.BlockState;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantment.Rarity;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.config.ModConfig;
 
-@Mod(modid = InfiniteFallBoots.MODID, name = InfiniteFallBoots.NAME, version = InfiniteFallBoots.VERSION)
-@Mod.EventBusSubscriber(modid = InfiniteFallBoots.MODID)
+@Mod(value = InfiniteFallBoots.MODID)
+@Mod.EventBusSubscriber(modid = InfiniteFallBoots.MODID, bus = Mod.EventBusSubscriber.Bus.MOD) // mod bus for registry
 public class InfiniteFallBoots {
-	public static final String MODID = "infinitefallboots";
-	public static final String NAME = "[SBM] Infinite Fall";
-	public static final String VERSION = "1.0.0";
+    public static final String MODID = "infinitefallboots";
 
-	public static Logger logger;
+    public static final Enchantment INFINITE_FALLING = new ModEnchantment(Rarity.RARE, EnchantmentType.ARMOR_FEET, new EquipmentSlotType[] {EquipmentSlotType.FEET}).setRegistryName(new ResourceLocation(InfiniteFallBoots.MODID, "infinitefalling"));
+    
+    public InfiniteFallBoots() {
+        FallConfig.setupConfig();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, FallConfig.SERVER_CONFIG);
+    }
+    
+    @SubscribeEvent
+    public static void registerEnchants(Register<Enchantment> event) {
+        event.getRegistry().register(INFINITE_FALLING);
+    }
 
-	public static final EnchantmentInfiniteFall INFINITE_FALLING = new EnchantmentInfiniteFall();
+    @Mod.EventBusSubscriber(modid = InfiniteFallBoots.MODID)
+    public static class FallHandler {
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		logger = event.getModLog();
-	}
+        @SubscribeEvent
+        public static void onLanding(LivingFallEvent event) {
+            LivingEntity elb = event.getEntityLiving();
+            int level = EnchantmentHelper.getMaxEnchantmentLevel(INFINITE_FALLING, elb);
+            if(level > 0) {
+                if(event.getDistance() >= FallConfig.FALL_CONFIG.minTriggerHeight.get()) {
 
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
+                    boolean notObstructed = true;
+                    double impactPosition = 0;
 
-	}
-
-	@SubscribeEvent
-	public static void registerEnchants(Register<Enchantment> event) {
-		event.getRegistry().register(INFINITE_FALLING);
-	}
-
-	@SubscribeEvent
-	public static void onLanding(LivingFallEvent event) {
-		EntityLivingBase elb = event.getEntityLiving();
-		int level = EnchantmentHelper.getMaxEnchantmentLevel(INFINITE_FALLING, elb);
-		if(level > 0) {
-			if(event.getDistance() >= FallConfig.minTriggerHeight) {
-
-				boolean notObstructed = true;
-				double impactPosition = 0;
-
-				for(int i = (int) elb.posY + 2; i < elb.world.provider.getHeight(); i++) {
-					BlockPos pos = new BlockPos(elb.posX, i, elb.posZ);
-					IBlockState state = elb.world.getBlockState(pos);
-					if(state.isFullBlock() || state.isFullCube()) {
-						notObstructed = false;
-						impactPosition = i;
-						break;
-					}
-				}
+                    for(int i = (int) elb.posY + 2; i < elb.world.getHeight(); i++) {
+                        BlockPos pos = new BlockPos(elb.posX, i, elb.posZ);
+                        BlockState state = elb.world.getBlockState(pos);
+                        if(state.isNormalCube(elb.getEntityWorld(), pos)) {
+                            notObstructed = false;
+                            impactPosition = i;
+                            break;
+                        }
+                    }
 
 
-				if(notObstructed) {
-					elb.setPosition(elb.posX, elb.world.provider.getHeight() + FallConfig.heightToAdd, elb.posZ);
-					event.setDamageMultiplier(0);
-				} else {
-					elb.addVelocity(0, (impactPosition - elb.posY) / 2, 0);
-					elb.attackEntityFrom(DamageSource.GENERIC, FallConfig.damageOnImpact);
-					event.setDamageMultiplier(0);
-				}
-			}
-		}
-	}
-
-	@SubscribeEvent
-    public static void onConfigChanged(ConfigChangedEvent event) {
-        if(event.getModID().equals(InfiniteFallBoots.MODID)) {
-        	ConfigManager.sync(event.getModID(), Config.Type.INSTANCE);
+                    if(notObstructed) {
+                        elb.setPosition(elb.posX, elb.world.getHeight() + FallConfig.FALL_CONFIG.heightToAdd.get(), elb.posZ);
+                        event.setDamageMultiplier(0);
+                    } else {
+                        elb.addVelocity(0, (impactPosition - elb.posY) / 2, 0);
+                        elb.attackEntityFrom(DamageSource.GENERIC, FallConfig.FALL_CONFIG.damageOnImpact.get().floatValue());
+                        event.setDamageMultiplier(0);
+                    }
+                }
+            }
         }
-	}
+
+    }
+    
+    public static class ModEnchantment extends Enchantment {
+
+        protected ModEnchantment(Enchantment.Rarity rarityIn, EnchantmentType typeIn, EquipmentSlotType[] slots) {
+            super(rarityIn, typeIn, slots);
+        }
+
+    }
+    
+    public static class FallConfig {
+        
+        public static FallConfig FALL_CONFIG = null;
+
+        public static ForgeConfigSpec SERVER_CONFIG = null;
+
+        public static void setupConfig() {
+            final Pair<FallConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(FallConfig::new);
+            SERVER_CONFIG = specPair.getRight();
+            FALL_CONFIG = specPair.getLeft();
+        }
+        
+        /* Non-static members */
+        
+        public final ForgeConfigSpec.IntValue minTriggerHeight;
+        public final ForgeConfigSpec.IntValue heightToAdd;
+        public final ForgeConfigSpec.DoubleValue damageOnImpact;
+
+        protected FallConfig(ForgeConfigSpec.Builder builder) {
+            this.minTriggerHeight = builder.comment("The minimum height to fall initially in order to trigger infinite fall").defineInRange("min_trigger_height", 2, 0, Integer.MAX_VALUE);
+            this.heightToAdd = builder.comment("Height that will be added to the world height for the infinite fall").defineInRange("height_to_add", 50, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            this.damageOnImpact = builder.comment("Amount of damage to deal when impacting a block above you").defineInRange("damage_on_impact", 0.5F, 0, Double.MAX_VALUE);
+        }
+
+    }
+
 }
